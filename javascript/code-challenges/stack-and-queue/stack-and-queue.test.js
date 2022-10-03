c9c4d6010e675d56c4d088e6eeb754f43030acc5
Kris Dunning <dunning.kris@gmail.com>
2022-10-02 22:53:34 -0700

    code challenge 10 stack and queue tests complete

--- javascript/code-challenges/stack-and-queue/stack-and-queue.test.js
+++ javascript/code-challenges/stack-and-queue/stack-and-queue.test.js
@@ -2,27 +2,19 @@
 
 let {Stack,Queue} = require ('./index');
 
-// Calling pop or peek on empty stack raises exception
-
-// let stack= new Stack;
-// console.log (stack);
-// console.log(stack.isEmpty());
-// stack.push(1);
-// console.log(stack);
-// stack.push(2);
-// console.log(stack);
-// console.log('peek: ',stack.peek());
-// console.log('popped Value: ',stack.pop());
-// console.log(stack);
-// console.log(stack.isEmpty());
-// console.log('peek: ',stack.peek());
-
 describe ('Stacks', ()=>{
 
   it('Can successfully instantiate an empty stack',()=>{
     let stack = new Stack;
     expect(stack).toBeInstanceOf(Stack);
     expect(stack.top).toBe(null);
+  });
+
+  it('Can successfully call isEmpty',()=>{
+    let stack = new Stack;
+    expect(stack.isEmpty()).toBe(true);
+    stack.push(1);
+    expect(stack.isEmpty()).toBe(false);
   });
 
   it('Can successfully push onto a stack', ()=>{
@@ -62,21 +54,81 @@ describe ('Stacks', ()=>{
     stack.push(2);
     expect(stack.peek()).toBe(2);
   });
-
-
+  it('Calling pop or peek on empty stack raises exception',()=>{
+    let stack = new Stack;
+    expect(() => {
+      stack.peek();
+    }).toThrow();
+    expect(() => {
+      stack.pop();
+    }).toThrow();
+  });
 });
 
+describe ('Queue', ()=>{
 
-// Can successfully enqueue into a queue
-// Can successfully enqueue multiple values into a queue
-// Can successfully dequeue out of a queue the expected value
-// Can successfully peek into a queue, seeing the expected value
-// Can successfully empty a queue after multiple dequeues
-// Can successfully instantiate an empty queue
-// Calling dequeue or peek on empty queue raises exception
+  it('Can successfully instantiate an empty queue',()=>{
+    let queue = new Queue;
+    expect(queue).toBeInstanceOf(Queue);
+    expect(queue.front).toBe(null);
+  });
 
+  it('Can successfully call isEmpty',()=>{
+    let queue = new Queue;
+    expect(queue.isEmpty()).toBe(true);
+    queue.enqueue(1);
+    expect(queue.isEmpty()).toBe(false);
+  });
 
+  it('Can successfully enqueue into a queue',()=>{
+    let queue = new Queue;
+    queue.enqueue(1);
+    expect(queue.front.value).toBe(1);
+  });
 
+  it('Can successfully enqueue multiple values into a queue',()=>{
+    let queue = new Queue;
+    queue.enqueue(1);
+    expect(queue.front.value).toBe(1);
+    queue.enqueue(2);
+    expect(queue.front.value).toBe(1);
+    expect(queue.front.next.value).toBe(2);
+  });
+
+  it('Can successfully dequeue out of a queue the expected value',()=>{
+    let queue = new Queue;
+    queue.enqueue(1);
+    queue.enqueue(2);
+    expect(queue.dequeue()).toBe(1);
+    expect(queue.front.value).toBe(2);
+  });
+
+  it('Can successfully peek into a queue, seeing the expected value',()=>{
+    let queue = new Queue;
+    queue.enqueue(1);
+    queue.enqueue(2);
+    expect(queue.peek()).toBe(1);
+  });
+
+  it('Can successfully empty a queue after multiple dequeues',()=>{
+    let queue = new Queue;
+    queue.enqueue(1);
+    queue.enqueue(2);
+    expect(queue.dequeue()).toBe(1);
+    expect(queue.dequeue()).toBe(2);
+    expect(queue.front).toBeNull();
+  });
+
+  it('Calling dequeue or peek on empty queue raises exception',()=>{
+    let queue = new Queue;
+    expect(() => {
+      queue.peek();
+    }).toThrow();
+    expect(() => {
+      queue.dequeue();
+    }).toThrow();
+  });
+});
 
 
 
